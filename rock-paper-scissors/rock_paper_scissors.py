cd5ca9c3f6c633b4252ed8fa7fec7adf496a514a
naimatullahRezaee <naimatullahrezaee@gmail.com>
2024-02-06 12:07:53 +0430

    complete the game

--- rock-paper-scissors/rock_paper_scissors.py
+++ rock-paper-scissors/rock_paper_scissors.py
@@ -16,4 +16,22 @@ while True:
     random_number = random.randint(0.2)
     # rock: 0 , paper: 1 ,scissors: 2
 
-  
+    computer_pick = options[random_number]
+    print(f"Computer Pick: {computer_pick}.")
+
+    if user_input == "rock" and computer_pick == "scissors":
+        print("you won!")
+        user_win += 1
+    elif user_input == "paper" and computer_pick == "rock":
+        print("you won!")
+        user_win += 1
+    elif user_input == "scissors" and computer_pick == "paper":
+        print("you won!")
+        user_win += 1
+    else:
+        print("you lost!")
+        computer_win += 1
+    
+print(f"You won! {user_win} times.")
+print(f"Computer won! {computer_win} times.")
+print("Goodbye")
