f8872d3973d4f300e8f4eb59058a75142cb6def5
naimatullahRezaee <naimatullahrezaee@gmail.com>
2024-02-06 11:31:04 +0430

    complete the guessing grame

--- number guessing/num_guessing.py
+++ number guessing/num_guessing.py
@@ -11,4 +11,26 @@ else:
     quit()
 
 random_number = random.randint(0, top_of_range)
+
+
+guesses = 0
+
+while True:
+    guesses += 1
+    user_guess = input("Make a guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print('Please type a number next time.')
+        continue
+    if user_guess == random_number:
+        print('You got it!')
+        break
+    else:
+        if user_guess < random_number:
+            print('Too low!')
+        else:
+            print('Too high!')
+
+print(f"You got it in {guesses} guesses")   
          
