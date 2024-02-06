388fe7bdc893bfef55bcaa9547286715e69624e1
naimatullahRezaee <naimatullahrezaee@gmail.com>
2024-02-06 10:04:11 +0430

    first quiz code

---
+++ quize/quiz.py
@@ -0,0 +1,46 @@
+print("Welcome to the Quiz!")
+
+
+playing = input("Do you want to play the game? (y/n): ")
+score = 0
+
+if playing.lower() != "yes":
+    quit()
+
+print("Okay!, Let's play the game 😊 ")
+
+answer = input("What does CPU stand for? ")
+
+if answer.lower() == "centeral processing unit":
+    print("Correct!")
+    score +=1
+
+else: 
+    print("Wrong!")
+    # print("CPU stands for Centeral Processing Unit")
+
+
+answer = input("What does RAM stand for? ")
+
+if answer.lower() == "random access memory":
+    print("Correct!")
+    score +=1
+
+else: 
+    print("Wrong!")
+    # print("CPU stands for Radom Access Memory")
+
+answer = input("What does GPU stand for? ")
+
+if answer.lower() == "graphic processing unit":
+    print("Correct!")
+    score +=1
+
+else: 
+    print("Wrong!")
+    # print("CPU stands for Graphic Processing Unit")
+
+
+print(f"You got {score} questions correct")
+
+    
