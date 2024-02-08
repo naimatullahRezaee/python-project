0343d1793ad555eae5544fa36e432e60dcef3337
naimatullahRezaee <naimatullahrezaee@gmail.com>
2024-02-08 09:58:04 +0430

    complete the code

--- password-manager/pass_manager.py
+++ password-manager/pass_manager.py
@@ -1,31 +1,51 @@
+from cryptography.fernet import Fernet
 
-pwd = input("What is the master password? ")
+
+# first run this code after that you can comment the following code
+'''
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)'''
+
+# this function sould write after the above function
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+
+key = load_key()
+fer = Fernet(key)
 
 
 def view():
     with open('password.txt', 'r') as f:
         for line in f.readlines():
             data = line.rstrip()
-            user,passw = data.split("|")
-            print(f" Username: {user}\n Password: {passw} ")
+            user, passw = data.split("|")
+            print("User:", user, "| Password:",fer.decrypt(passw.encode()).decode())
+
 
 def add():
-    name = input("Account name: ")
-    password = input("Password: ")
+    name = input('Account Name: ')
+    pwd = input("Password: ")
 
     with open('password.txt', 'a') as f:
-        f.write(f"{name} | {password} \n")
+        f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
 
 
 while True:
-    mode = input("Would you like to add a new password to or view existing one (view, add) or enter q for quit? ").lower()
-
-    if mode == "q" or mode == "quit":
+    mode = input(
+        "Would you like to add a new password or view existing ones (view, add), press q to quit? ").lower()
+    if mode == "q":
         break
+
     if mode == "view":
-        view() 
+        view()
     elif mode == "add":
         add()
     else:
-        print("Invalid password")
-    
+        print("Invalid mode.")
+        continue
