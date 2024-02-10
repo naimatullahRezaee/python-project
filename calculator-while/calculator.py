f5cc823f9dcf5c935550c8a891adb549cc71e01f
naimatullahRezaee <naimatullahrezaee@gmail.com>
2024-02-10 14:04:13 +0430

    complete all the project

--- calculator-while/calculator.py
+++ calculator-while/calculator.py
@@ -18,3 +18,20 @@ while True:
         continue
     num1 = float(input("Enter the first number: "))
     num2 = float(input("Enter the second number: "))
+    if choice == '1':
+        result = num1 + num2
+        print("Result: ", result)
+    elif choice == '2':
+        result = num1 - num2
+        print("Result: ", result)
+    elif choice == '3':
+        result = num1 * num2
+        print("Result: ", result)
+    elif choice == '4':
+        if num2 == 0:
+            print("Error: Division by zero is not allowed.")
+        else:
+            result = num1 / num2
+            print("Result: ", result)
+    else:
+        print("Invalid input. Please try again.")
