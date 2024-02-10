80a5302b7cc7d13ac513ae215a68e80acab49ced
naimatullahRezaee <naimatullahrezaee@gmail.com>
2024-02-10 13:59:20 +0430

    add new project

---
+++ calculator-while/calculator.py
@@ -0,0 +1,8 @@
+print("Welcome to the calculator!")
+while True:
+    print("Please select an operation:")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    print("5. Exit")
