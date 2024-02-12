a7e8dc2e6399b51b6c070a334e536cf5b80d1923
naimatullahRezaee <naimatullahrezaee@gmail.com>
2024-02-12 11:53:08 +0430

    add new funcationality

--- madlib-game/madelib.py
+++ madlib-game/madelib.py
@@ -2,16 +2,22 @@ with open('madlib-game/story.txt','r') as f:
     story = f.read()
 print(story)
 
-words = []
+words = set()
 start_word = -1
 
 target_start = "<"
 target_end = ">"
 
-for i , char in enumerate():
+for i , char in enumerate(story):
     if char == target_start:
         start_word = i
     if char == target_end and start_word != -1:
         word = story[start_word: i +1]
-        words.append(word)
+        words.add(word)
         start_word = -1
+
+answers = {}
+
+for word in words:
+    answer = input(f"Enter a word for {word} : ")
+    answers[word] = answer
