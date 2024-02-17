82e4a69f1648c993c0d8b73baa8f57a9d4d1d858
naimatullahRezaee <naimatullahrezaee@gmail.com>
2024-02-17 11:43:44 +0430

    add new project

---
+++ math-challanges/math.py
@@ -0,0 +1,17 @@
+import random
+import time
+
+OPERATORS = ["+", "-", "*"]
+MIN_OPERAND = 3
+MAX_OPERAND = 12
+TOTAL_PROBLEMS = 10
+
+
+def generate_problem():
+    left = random.randint(MIN_OPERAND, MAX_OPERAND)
+    right = random.randint(MIN_OPERAND, MAX_OPERAND)
+    operator = random.choice(OPERATORS)
+
+    expr = str(left) + " " + operator + " " + str(right)
+    answer = eval(expr)
+    return expr, answer
